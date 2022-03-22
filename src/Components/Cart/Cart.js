fda3777757c58ef193d0f436e6d44f3e23844dda
Arafat37 <arafatsarkar22@gmail.com>
2022-03-22 23:25:12 +0600

    added cart component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Cart = ({cart, setCart}) => {
+  return (
+    <div className='cart-side'>
+      <h4 className='text-center mt-4'>
+              Order Summary
+            </h4>
+        <div className='w-75 mx-auto my-5'>
+          <div>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: <span>$</span>{cart.reduce((previous, current) => previous + current.price, 0)}</p>
+            <p>Total Shipping Charge: <span>$</span>{cart.length * 20}</p>
+            <p>Tax: <span>$</span>{(cart.reduce((previous, current) => previous + current.price, 0) * .2).toFixed(2)}</p>
+            <h5>Grand Total: <span>$</span>{(cart.reduce((previous, current) => previous + current.price, 0)) + (cart.reduce((previous, current) => previous + current.price, 0) * .2)}</h5>
+          </div>
+        </div>
+        <div className='d-flex flex-column align-items-center'>
+          <button onClick={() => setCart([])} className='cart-btn rounded my-1 py-1'>Clear Cart <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon></button>
+          <button className='review-btn rounded my-1 py-1'>Review Order <FontAwesomeIcon icon={faArrowRight} /></button>
+        </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,6 @@
-import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from './Product/Product';
 import './Products.css'
 
@@ -32,25 +32,7 @@ const Products = () => {
             ></Product>)
         }
       </div>
-
-      <div className='cart-side'>
-        <h4 className='text-center mt-4'>
-              Order Summary
-            </h4>
-        <div className='w-75 mx-auto my-5'>
-          <div>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: <span>$</span>{cart.reduce((previous, current) => previous + current.price, 0)}</p>
-            <p>Total Shipping Charge: <span>$</span>{cart.length * 20}</p>
-            <p>Tax: <span>$</span>{(cart.reduce((previous, current) => previous + current.price, 0) * .2).toFixed(2)}</p>
-            <h5>Grand Total: <span>$</span>{(cart.reduce((previous, current) => previous + current.price, 0)) + (cart.reduce((previous, current) => previous + current.price, 0) * .2)}</h5>
-          </div>
-        </div>
-        <div className='d-flex flex-column align-items-center'>
-          <button onClick={() => setCart([])} className='cart-btn rounded my-1 py-1'>Clear Cart <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon></button>
-          <button className='review-btn rounded my-1 py-1'>Review Order <FontAwesomeIcon icon={faArrowRight} /></button>
-        </div>
-      </div>
+      <Cart cart={cart} setCart={setCart}></Cart>
   </div>
   );
 };
